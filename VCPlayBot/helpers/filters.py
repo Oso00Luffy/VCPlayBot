36a24e786f1a3270288c0dd1f39c4e3bdb6c7cee
Osama Jaradat <38565843+Oso00Luffy@users.noreply.github.com>
2025-03-23 03:43:14 +0300

    Update filters.py

--- VCPlayBot/helpers/filters.py
+++ VCPlayBot/helpers/filters.py
@@ -1,15 +1,12 @@
-from typing import List
-from typing import Union
-
+from typing import List, Union
 from pyrogram import filters
-
 from VCPlayBot.config import COMMAND_PREFIXES
 
+# تعديل الفلتر بحيث يستخدم filters.update.edited بدلاً من filters.edited
 other_filters = filters.group & ~filters.via_bot & ~filters.forwarded
 other_filters2 = (
-    filters.private & ~filters.edited & ~filters.via_bot & ~filters.forwarded
+    filters.private & ~filters.update.edited & ~filters.via_bot & ~filters.forwarded
 )
-
 
 def command(commands: Union[str, List[str]]):
     return filters.command(commands, COMMAND_PREFIXES)
