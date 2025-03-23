5a8cd9c31231d62d1f0c6cef9ed6dda101c50df7
Osama Jaradat <38565843+Oso00Luffy@users.noreply.github.com>
2025-03-23 03:50:15 +0300

    Update filters.py

--- VCPlayBot/helpers/filters.py
+++ VCPlayBot/helpers/filters.py
@@ -2,11 +2,9 @@ from typing import List, Union
 from pyrogram import filters
 from VCPlayBot.config import COMMAND_PREFIXES
 
-# تعديل الفلتر بحيث يستخدم filters.update.edited بدلاً من filters.edited
+# إزالة filters.update.edited لأنه غير موجود في pyrogram
 other_filters = filters.group & ~filters.via_bot & ~filters.forwarded
-other_filters2 = (
-    filters.private & ~filters.update.edited & ~filters.via_bot & ~filters.forwarded
-)
+other_filters2 = filters.private & ~filters.edited & ~filters.via_bot & ~filters.forwarded
 
 def command(commands: Union[str, List[str]]):
     return filters.command(commands, COMMAND_PREFIXES)
